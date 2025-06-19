87602a5e48de118caeab652286114c76e3bced95
Akash <akashkewat99@gmail.com>
2025-06-19 08:59:45 +0530

    completed

--- June16/12/index.js
+++ June16/12/index.js
@@ -3,59 +3,40 @@ const library = {
         { title: "The Hobbit", author: "J.R.R. Tolkien", year: 1937 }
     ],
 
-
     addBook(book) {
+        const { title, author, year } = book;
 
-        if (
-            typeof book.title !== "string" ||
-            typeof book.author !== "string" ||
-            typeof book.year !== "number"
-        ) {
-            console.log("Book information is incomplete or invalid.");
-            return;
-        }
-
-
-        if (this.findBookByTitle(book.title)) {
-            console.log(`Book titled "${book.title}" already exists.`);
+        if (typeof title !== "string" || typeof author !== "string" || typeof year !== "number") {
+            console.log("Book information is incomplete or invalid");
             return;
         }
 
         this.books.push(book);
-        console.log(`Book titled "${book.title}" added successfully.`);
-    },
+        console.log("Added sucessfully.");
 
+    },
 
     findBookByTitle(title) {
         return this.books.find(book => book.title === title);
     },
 
-
     removeBook(title) {
-        const index = this.books.findIndex(book => book.title === title);
+        const index = this.books.findIndex(book => book.title === title)
         if (index !== -1) {
-            const removed = this.books.splice(index, 1);
-            console.log(`Removed: "${removed[0].title}"`);
+            this.books.splice(index, 1);
+            console.log("Removed Sucessfully");
         } else {
-            console.log(`Book titled "${title}" not found.`);
+            console.log("Book not found.");
+
         }
     }
-};
+}
+
 
 
 library.addBook({ author: "George Orwell", year: 1949 });
 
-
-console.log("Total books:", library.books.length);
-
-
 library.addBook({ title: "1984", author: "George Orwell", year: 1949 });
 
-
-console.log("Total books:", library.books.length);
-
-
-library.removeBook("1984");
-
-
-console.log("Total books:", library.books.length); 
+console.log("Total Books:", library.books.length);
+console.log("All Books:", library.books);
